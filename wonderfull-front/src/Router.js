bfdaa35f67f47f3f03a7921efe284e1923aea890
nancy27vazquez <nevadg@outlook.com>
2019-11-27 14:45:48 -0600

    Adding components to Router.js

--- wonderfull-front/src/Router.js
+++ wonderfull-front/src/Router.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-
-// Import here whatever component you may need //
+import Login from "";
+import Signup from "";
+import Feed from "";
+import Recommendation from "";
+import NextTrip from "";
+import Favorites from "";
+import Profile from "";
+// Import above whatever component you may need //
 
 const Router = () => (
   <Switch>
@@ -14,11 +20,20 @@ const Router = () => (
     <Route exact path="/signup">
       <Signup />
     </Route>
-    <Route path="/home">
-      <Home />
+    <Route path="/feed">
+      <Feed />
     </Route>
     <Route path="/recommendation">
       <Recommendation />
+    </Route>
+    <Route path="/next-trip">
+      <NextTrip />
+    </Route>
+    <Route path="/favorites">
+      <Favorites />
+    </Route>
+    <Route path="/profile">
+      <Profile />
     </Route>
   </Switch>
 );
