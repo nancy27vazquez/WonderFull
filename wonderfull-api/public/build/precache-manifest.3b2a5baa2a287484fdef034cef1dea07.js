bd0081165f4ccc34506f5ed6b855ada916b331c2
nancy27vazquez <nevadg@outlook.com>
2019-12-12 20:58:03 -0600

    build actualization

---
+++ wonderfull-api/public/build/precache-manifest.3b2a5baa2a287484fdef034cef1dea07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf38c04f54bf542cb7e154e83542e06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2635a0575a8fee385ae",
+    "url": "/static/css/2.f18f804e.chunk.css"
+  },
+  {
+    "revision": "78409717f28867707cf8",
+    "url": "/static/css/main.66659496.chunk.css"
+  },
+  {
+    "revision": "e2635a0575a8fee385ae",
+    "url": "/static/js/2.11e531e8.chunk.js"
+  },
+  {
+    "revision": "78409717f28867707cf8",
+    "url": "/static/js/main.3487a66d.chunk.js"
+  },
+  {
+    "revision": "320ce27d9e90e88952d3",
+    "url": "/static/js/runtime-main.35b5e400.js"
+  }
+]);
