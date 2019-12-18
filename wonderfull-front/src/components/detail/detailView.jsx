5cd16d22f8abb47474a35991441f077b94e96e56
nancy27vazquez <nevadg@outlook.com>
2019-12-17 19:30:09 -0600

    Last version changes of frontend

--- wonderfull-front/src/components/detail/detailView.jsx
+++ wonderfull-front/src/components/detail/detailView.jsx
@@ -1,9 +1,42 @@
+import React from "react";
+
+const WonderDetail = () => {
+  return (
+    <div>
+      <div class="uk-child-width-1-1@s uk-text-center uk-light" uk-grid>
+        <div>
+          <div class="uk-background-cover uk-height-medium uk-panel uk-flex uk-flex-center uk-flex-middle auth-bg">
+            <div class="uk-overlay-primary uk-position-cover"></div>
+            <div class="uk-overlay uk-position-center uk-light">
+              <h2 className="uk-margin-remove-bottom">
+                Yosemite National Park
+              </h2>
+              <p className="uk-text-lead uk-margin-remove-top">
+                California, United States
+              </p>
+              <div class="">
+                <span
+                  className="save-wonder-button uk-margin-small-right"
+                  uk-icon="star"
+                  id="tofavorites"
+                ></span>
+                <span
+                  className="save-wonder-button uk-margin-small-right"
+                  uk-icon="heart"
+                  id="tonexttrip"
+                ></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WonderDetail;
+
 /*
-name
-country
-state
-make fav button
-make next trip button
 address/map
 tags
 rate 
--- wonderfull-front/src/components/favorites/Favorites.jsx
+++ wonderfull-front/src/components/favorites/Favorites.jsx
@@ -1,12 +1,88 @@
 import React from "react";
 import Menu from "../common/Nav";
 import Footer from "../common/Footer";
+import WonderDetail from "../detail/detailView";
 
 const Favorites = () => {
   return (
     <div>
       <Menu />
-      <p>favorites</p>
+      <div
+        class="uk-position-relative uk-visible-toggle"
+        tabindex="-1"
+        uk-slideshow="animation: fade"
+        uk-height-viewport="min-height: 300"
+      >
+        <ul class="uk-slideshow-items" uk-height-viewport="offset-top: true">
+          <li>
+            <img
+              src="https://getuikit.com/docs/images/photo.jpg"
+              alt=""
+              uk-cover
+            />
+            <div class="uk-position-center uk-position-small uk-text-center uk-light">
+              <h2 class="uk-margin-remove">Center</h2>
+              <p class="uk-margin-remove">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </div>
+          </li>
+          <li>
+            <img
+              src="https://getuikit.com/docs/images/dark.jpg"
+              alt=""
+              uk-cover
+            />
+            <div class="uk-position-bottom uk-position-medium uk-text-center uk-light">
+              <h3 class="uk-margin-remove">Bottom</h3>
+              <p class="uk-margin-remove">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </div>
+          </li>
+          <li>
+            <img
+              src="https://getuikit.com/docs/images/light.jpg"
+              alt=""
+              uk-cover
+            />
+            <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center">
+              <h3 class="uk-margin-remove">Overlay Bottom</h3>
+              <p class="uk-margin-remove">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </div>
+          </li>
+          <li>
+            <img
+              src="https://getuikit.com/docs/images/dark.jpg"
+              alt=""
+              uk-cover
+            />
+            <div class="uk-overlay uk-overlay-default uk-position-bottom-right uk-position-small">
+              <h3 class="uk-margin-remove">Overlay Bottom Right</h3>
+              <p class="uk-margin-remove">Lorem ipsum dolor sit amet.</p>
+            </div>
+          </li>
+        </ul>
+
+        <div class="uk-light">
+          <a
+            class="uk-position-center-left uk-position-small uk-hidden-hover"
+            href="#"
+            uk-slidenav-previous
+            uk-slideshow-item="previous"
+          ></a>
+          <a
+            class="uk-position-center-right uk-position-small uk-hidden-hover"
+            href="#"
+            uk-slidenav-next
+            uk-slideshow-item="next"
+          ></a>
+        </div>
+      </div>
+
+      <WonderDetail />
       <Footer />
     </div>
   );
